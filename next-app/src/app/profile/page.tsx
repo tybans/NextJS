8f509e875ec92f8ed762c6c809846ef560021d36
Taiyab Ansari <taiyab0523@gmail.com>
2024-04-17 19:26:34 +0530

    WIP: folder structuring for frontend is done

---
+++ next-app/src/app/profile/page.tsx
@@ -0,0 +1,12 @@
+export default function ProfilePage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Profile</h1>
+      <hr />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo,
+        numquam.
+      </p>
+    </div>
+  );
+}
